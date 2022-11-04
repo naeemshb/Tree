44164d2ff52ceaa8d2446b5333b054d56dbb5ab8
Naeem <naeem.shahaby@gmail.com>
2022-11-03 22:48:52 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -4,10 +4,13 @@ node_possibilities = ["op", "num"]
 
 operators = ["plus", "minus", "mul", "div", "min", "max", "OR", "AND", "XOR"]
 
-
-root = Node (["op", "plus"])
+root = Node(["op", "plus"])
 a = Node(["op", "div"], parent=root)
-b = Node(["num", 5], parent=root)
+b = Node(["op", "minus"], parent=root)
+e = Node(["op", 'max'], parent=b)
+g = Node(["num", 11], parent=e)
+h = Node(["num", 13], parent=e)
+f = Node(["num", 4], parent=b)
 c = Node(["num", 3], parent=a)
 d = Node(["num", 2], parent=a)
 
@@ -15,40 +18,62 @@ root.left = a
 root.right = b
 a.left = c
 a.right = d
+b.left = e
+b.right = f
+e.left = g
+e.right = h
 
-print(RenderTree(root))
+#print(RenderTree(root))
+for pre, fill, node in RenderTree(root):
+      print("%s%s" % (pre, node.name))
 
 val = root.name
-#print(val[0])
-#print(val[1])
-
-#print(root.children)
 
 
-def my_plus_function (a, b):
+print(val)
+# print(val[1])
+
+# print(root.children)
+
+
+def my_plus_function(a, b):
     return a + b
 
-def my_div_function (a, b):
+def my_minus_function(a, b):
+    return (a - b)
+
+def my_max_function(a, b):
+
+    res = max(a,b)
+    return res
+
+
+def my_div_function(a, b):
     return float(a) / float(b)
 
-def evaluate (mytree):
+
+def evaluate(mytree):
     if mytree == None:
-        print ("This should not happen")
-        return 0    # throw an exception here; this should not happen
+        print("This should not happen")
+        return 0  # throw an exception here; this should not happen
 
     val = mytree.name
     if val[0] == "num":
-        #print("returning a real number now: %f", val[1])
+        # print("returning a real number now: %f", val[1])
         return val[1]
-    
+
     result = 0
     match val[1]:
         case "plus":
             result += my_plus_function(evaluate(mytree.left), evaluate(mytree.right))
         case "div":
             result += my_div_function(evaluate(mytree.left), evaluate(mytree.right))
-
+        case "minus":
+            result += my_minus_function(evaluate(mytree.left), evaluate(mytree.right))
+        case "max":
+            result += my_max_function(evaluate(mytree.left), evaluate(mytree.right))
     return result
+
 
 print("\n\n evaluating now ...\n\n")
 
